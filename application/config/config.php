4c43f0df36641d9302c744c78bf525fa7b1fb85f
Hirekaan Micheal Hemen <hirekaanmicheal@gmail.com>
2016-09-29 14:20:18 +0100

    adding APPPATH\tmp folder for sessions

--- application/config/config.php
+++ application/config/config.php
@@ -369,12 +369,13 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_sessions';
-$config['sess_match_ip'] = TRUE;
-$config['sess_time_to_update'] = 300;
+$config['sess_driver'] 			= 'files';
+$config['sess_cookie_name'] 	= 'ci_session';
+$config['sess_expiration'] 		= 7200;
+$config['sess_save_path'] 		= APPPATH ."/tmp";
+$config['sess_expire_on_close']	= FALSE;
+$config['sess_match_ip'] 		= TRUE;
+$config['sess_time_to_update'] 	= 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
