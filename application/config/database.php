c62de6b5899eb36603651a46d352531168f54553
Hirekaan Micheal Hemen <hirekaanmicheal@gmail.com>
2016-09-29 14:30:25 +0100

    sessions final update

--- application/config/database.php
+++ application/config/database.php
@@ -70,8 +70,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-#$active_group = 'heroku';
-$active_group = 'localhost';
+$active_group = 'heroku';
+#$active_group = 'localhost';
 $query_builder = TRUE;
 
 $db['heroku'] = array(
