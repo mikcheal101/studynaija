a7bff288f6bfe5a0eebed7d6f6ae1058c1aeb77a
Hirekaan Micheal Hemen <hirekaanmicheal@gmail.com>
2016-09-29 08:23:18 +0100

    sessions directory updated (1)

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'localhost';
+$active_group = 'heroku';
 $query_builder = TRUE;
 
 $db['heroku'] = array(
