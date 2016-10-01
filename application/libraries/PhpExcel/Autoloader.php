9c0d45d24566790b9e11b8adf444f1088161f226
Hirekaan Micheal Hemen <hirekaanmicheal@gmail.com>
2016-10-01 20:01:00 +0100

    PHPExcel updated

--- application/libraries/PhpExcel/Autoloader.php
+++ application/libraries/PhpExcel/Autoloader.php
@@ -48,11 +48,14 @@ class PHPExcel_Autoloader
             spl_autoload_register('__autoload');
         }
         // Register ourselves with SPL
+		return spl_autoload_register(array('PHPExcel_Autoloader', 'load'));
+		/*
         if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
-            return spl_autoload_register(array('PHPExcel_Autoloader', 'load'));
+            return spl_autoload_register(array('PHPExcel_Autoloader', 'load'), true, true);
         } else {
             return spl_autoload_register(array('PHPExcel_Autoloader', 'load'));
         }
+		*/
     }
 
     /**
